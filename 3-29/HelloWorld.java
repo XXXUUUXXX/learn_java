23011798536bd2849e603b65ef707b87aec2fd03
tian <963766613@qq.com>
2018-03-29 19:43:38 +0800

    03-29

--- 3-29/HelloWorld.java
+++ 3-29/HelloWorld.java
@@ -4,6 +4,8 @@
         2.定义主方法
         3.一条命令，控制台输出
 */
+//HelloWorld是类名称，必须与源文件名称相同
+//类的内容存放在大括号中
 public class HelloWorld{
     //main是程序主方法，固定格式，程序的入口
     public static void main(String[] args){
